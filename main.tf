9ee1c6747d8da50cc7e6781a2a8c3fe2bd5a763a
VJKumar08 <vijayjack47@gmail.com>
2025-04-21 22:49:59 +0530

    tesr4

--- main.tf
+++ main.tf
@@ -2,29 +2,18 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "ami_value" {
-  description = "New AMI value"
-  default = "ami-0e449927258d45bc4"
-}
-
-variable "instance_type" {
-  description = "new instance type"
-  default = "t2.micro"
-}
-
-variable "key_value" {      
-    description = "key value"
-    default = "Vijay-Test-Key-pair"  
-}
-
 resource "aws_instance" "Newterraform" {
   ami = var.ami_value
   instance_type = var.instance_type
   key_name = var.key_value
+
+    tags = {
+    name = "instance"
+  }
 }
 
 resource "aws_s3_bucket" "my_first_bucket" {
-  bucket = "your-unique-bucket-name-again" # Replace with a globally unique name
+  bucket = var.aws_s3_bucket 
 
   tags = {
     Environment = "dev"
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "ami_value" {
+  description = "New AMI value"
+  default = "ami-0e449927258d45bc4"
+}
+
+variable "instance_type" {
+  description = "new instance type"
+  default = "t2.micro"
+}
+
+variable "key_value" {      
+    description = "key value"
+    default = "Vijay-Test-Key-pair"  
+}
+
+variable "aws_s3_bucket" {
+  description = "S3 bucket"
+  default = "terraform_s3_bucket"
+}
